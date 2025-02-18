d435b788bc0fd3c713c769f5be86a4647dcb2cb0
Mauricio Rogerio Obenaus <mobenaus@gmail.com>
2025-02-17 21:39:08 -0300

    Correção esquecimento de gravar arquivo txt com a cotação

--- client/client.go
+++ client/client.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -18,27 +20,62 @@ type Cotacao struct {
 const serverApiTimeout = 300 * time.Millisecond
 
 func main() {
+
+	cotacao, err := getCotacao()
+	if err != nil {
+		log.Fatalln("Erro ao buscar a cotação do server", err)
+	}
+
+	err = saveCotacao(cotacao)
+	if err != nil {
+		log.Fatalln("Erro ao gravar a cotação no arquivo", err)
+	}
+
+	fmt.Printf("Cotação do dolar é: %s\n", cotacao.Dolar)
+
+}
+
+func getCotacao() (Cotacao, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), serverApiTimeout)
 	defer cancel()
 	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
 	if err != nil {
-		log.Fatalln("Problema para criar a request", err)
+		return Cotacao{}, err
 	}
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		log.Fatalln("Problema para executar a request", err)
+		return Cotacao{}, err
 	}
 	defer res.Body.Close()
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		log.Fatalln("Problema para ler o retorno da request", err)
+		return Cotacao{}, err
 	}
 	var cotacao Cotacao
 	err = json.Unmarshal(body, &cotacao)
 	if err != nil {
-		log.Fatalln("Problema no formato do retorno", err)
+		return Cotacao{}, err
+	}
+	return cotacao, nil
+}
+
+func saveCotacao(cotacao Cotacao) error {
+	file, err := os.Create("cotacao.txt")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	tmp := template.New("Cotacao")
+	tmp, err = tmp.Parse("Dólar: {{.Dolar}}")
+	if err != nil {
+		return err
+	}
+	err = tmp.Execute(file, cotacao)
+	if err != nil {
+		return err
 	}
 
-	fmt.Printf("Cotação do dolar é: %s\n", cotacao.Dolar)
+	return nil
 
 }
